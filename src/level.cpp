feaf85e28c35300b2ace6ae8a6c37574df3ec561
Giffi <miklas.karjalainen@gmail.com>
2025-08-01 18:31:04 +0300

    Method to see if a rect overlaps with the level

--- src/level.cpp
+++ src/level.cpp
@@ -1,15 +1,61 @@
 #include "level.hpp"
 #include "player.hpp"
 #include "systems/texture_cacher.hpp"
+#include "vec2i.hpp"
 
-constexpr struct {
-	int x, y;
-} TileSize = {32, 32};
-
+#include <iostream>
 #include <stdexcept>
 #include <string>
+#include <utility>
+
+constexpr Vec2i TileSize = {32, 32};
+constexpr Vec2i TileDimensions = {20, 10};
 
 ILevel::ILevel() {}
+
+Tile ILevel::get_overlapping_tile(Rectangle rect) const
+{
+	const std::string_view level_data = get_level_data();
+	const auto [tile_index, rect_grid_position] =
+		[&rect]() -> std::pair<int, Vec2i> {
+		const int tiled_x = static_cast<int>(rect.x / TileSize.x);
+		const int tiled_y = static_cast<int>(rect.y / TileSize.y);
+		const int tile_index = tiled_y * TileDimensions.x + tiled_x;
+		return std::pair{tile_index, Vec2i{tiled_x, tiled_y}};
+	}();
+
+	for (int offx = -1; offx <= 1; offx++) {
+		for (int offy = -1; offy <= 1; offy++) {
+			// @TODO: wrapping
+			int level_index = tile_index + offx + (offy * TileDimensions.x);
+			if (level_index >= (TileDimensions.x * TileDimensions.y)) {
+				continue;
+			}
+			if (level_index < 0) {
+				continue;
+			}
+
+			const Tile current_tile =
+				static_cast<Tile>(level_data[level_index]);
+			if (current_tile == Tile::AIR) {
+				continue;
+			}
+
+			Rectangle hitbox = {
+				static_cast<float>((rect_grid_position.x + offx) * TileSize.x),
+				static_cast<float>((rect_grid_position.y + offy) * TileSize.y),
+				static_cast<float>(TileSize.x),
+				static_cast<float>(TileSize.y)
+			};
+
+			if (CheckCollisionRecs(rect, hitbox)) {
+				return current_tile;
+			}
+		}
+	}
+
+	return Tile::AIR;
+}
 
 void ILevel::draw() const
 {
--- src/level.hpp
+++ src/level.hpp
@@ -1,9 +1,10 @@
 #pragma once
+#include <raylib.h>
 #include <string_view>
 
 class Player;
 
-enum class Tiles : char { AIR = '.', BLOCK = '#' };
+enum class Tile : char { AIR = '.', BLOCK = '#' };
 
 class ILevel {
 public:
@@ -11,6 +12,10 @@ public:
 
 	virtual ~ILevel() {};
 	virtual void update(const Player& player) = 0;
+
+	// Only returns the first tile that is being overlapped with!
+	// Returns AIR tile if no other tile is being overlapped!
+	Tile get_overlapping_tile(Rectangle rect) const;
 
 protected:
 	virtual std::string_view get_level_data() const = 0;
